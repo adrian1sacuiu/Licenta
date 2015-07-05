6c811647a57a66b0860c4c0329c1261a0484fcc4
silviu <silviuhent@yahoo.com>
2015-07-05 21:28:49 +0300

    new ui
    complaints

--- AssetManagement/src/main/webapp/resources/scripts/app.js
+++ AssetManagement/src/main/webapp/resources/scripts/app.js
@@ -164,42 +164,26 @@ function newRequest(id){
 	});
 }
 function createComplaintAsset(assetId){
-//	xhr = $.ajax({
-//		type : 'POST',
-//		url : window.username+'/createComplaint?idAsset='+assetId,
-//		//url: 'availableAssets',
-//		data : '',
-//		success : function(data) {
-//			var a = "";
-			//$('#content').append('<div id="asset_error"></div><div id="info_asset"></div><div id="form_complaint"><select id="availableAssets"></select> <label>Error Description</label><textarea></textarea></div>');
 			$('.modal-body').html('<div class="form-group" id="edit_user_details" style="display: block;">'+
 					'<form role="form">'+
 					'<div class="form-group">'+
-					'<label class="col-sm-4 control-label" for="inputName1">Complaint Title:</label><div class="col-sm-8"><input class="form-control" placeholder="Title"/></div>'+
-					'<label class="col-sm-4 control-label" for="inputName1">Complaint Description:</label><div class="col-sm-8"><textarea class="form-control" placeholder="Description"></textarea></div>'+
+					'<label class="col-sm-4 control-label" for="inputName1">Complaint Title:</label><div class="col-sm-8"><input id="title_complaint" class="form-control" placeholder="Title"/></div>'+
+					'<label class="col-sm-4 control-label" for="inputName1">Complaint Description:</label><div class="col-sm-8"><textarea id="description_complaint" class="form-control" placeholder="Description"></textarea></div>'+
+					'<label class="col-sm-4 control-label" for="inputName1">Complaint Priority:</label><div class="col-sm-8"><select id="priority_complaint" class="form-control"><option value="0">Please Select</option><option value="Low">Low</option><option value="Medium">Medium</option><option value="High">High</option></select></div>'+
+					
+					
 					'</div></form></div><div class="clear"></div>');
-			$('.modal-footer').html('<button class="btn btn-default" data-dismiss="modal" onclick="postComplaint()" type="button">Send Complaint</button><button class="btn btn-default" data-dismiss="modal" type="button">Close</button>');
-//			if(data.status == "true"){
-//				getAssets(window.username);
-//				$('#info_asset').append(a);
-//				$('#asset_confirm_del').show();
-//				$('#asset_confirm_del').text(data.message);
-//				$('#asset_error').hide();
-//				//setTimeout(function() {$('#myModal').modal('hide');}, 3000);
-//			}else{   
-//				$('#asset_error').append(data.message[0]);
-//				$('#asset_error').show();
-//				//setTimeout(function() {$('#myModal').modal('hide');}, 3000);
-//			}
-//		}
-//	});
+			$('.modal-footer').html('<button class="btn btn-default" data-dismiss="modal" onclick="postComplaint('+assetId+');" type="button">Send Complaint</button><button class="btn btn-default" data-dismiss="modal" type="button">Close</button>');
+			
 }
 function postComplaint(assetId){
+	var title = $('#title_complaint').val();
+	var description = $('#description_complaint').val();
+	var priority = $('#priority_complaint').val();
 	xhr = $.ajax({
 		type : 'POST',
 		url : window.username+'/createComplaint?idAsset='+assetId,
-		//url: 'availableAssets',
-		data : '',
+		data : {title:title,description:description,priority:priority},
 		success : function(data) {
 			var a = "";
 			//$('#content').html('<div id="asset_error"></div><div id="info_asset"></div><div id="form_complaint"><select id="availableAssets"></select> <label>Error Description</label><textarea></textarea></div>');
